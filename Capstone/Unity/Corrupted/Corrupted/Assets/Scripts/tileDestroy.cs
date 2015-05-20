832dfd0c465193914a34545ad919a16a8c931ebb
hduuong <hungduong1309@gmail.com>
2015-05-20 14:16:09 -0700

    added healing animation, destroy animation, fix heal() infinite loop

---
+++ Capstone/Unity/Corrupted/Corrupted/Assets/Scripts/tileDestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tileDestroy : MonoBehaviour {
+	public float selfDestructTime;
+	// Use this for initialization
+	void Start () {
+		selfDestructTime = 0.25f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		selfDestructTime -= Time.deltaTime;
+		if(selfDestructTime <= 0){
+			Destroy(gameObject);
+		}
+	}
+}
